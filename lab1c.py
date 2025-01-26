3619dcad96ad6694878f50c636a3bc99b9cea0d1
Edison Leung <eleung41@myseneca.ca>
2025-01-25 21:42:49 -0500

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Edison Leung
+
+# Create object "name" and assign value "Isaac"
+name = 'Isaac'
+
+# Create object "age", assign value 72
+age = 72
+
+# print desired output converting integer object into string for print argument
+print(name + ' is ' + str(age) + ' years old!')
+
