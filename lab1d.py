c234dc1c2091f17f2de3de63859c3ebf5d50f497
Edison Leung <eleung41@myseneca.ca>
2025-01-26 03:31:53 -0500

    added lab1d.py

---
+++ lab1d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Edison Leung
+
+
+
+# Creates Create objects x, y, and z
+x = 10
+y = 2
+z = 5
+
+# Perform the calculation and store the result in a variable
+result = x + y * z
+
+# Print the output in the required format
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
